72e11ee84b72716d31b9e90b51f65bc2d595e17b
1SYF666 <2832195814@qq.com>
2023-11-05 23:22:19 +0800

    second

--- DataStructure/test.cpp
+++ DataStructure/test.cpp
@@ -67,41 +67,162 @@
 
 /* addword.c -- 使用fprintf(),fscanf(),和rewind()函数 */
 
-#define MAX 40
-int main()
+//#define MAX 40
+//int main()
+//{
+//	FILE* fp;
+//	char words[MAX];
+//
+//	if ((fp = fopen("words", "a+")) == NULL)
+//	{
+//		fprintf(stderr, "Can't open \"words\" file.\n");
+//		exit(1);
+//	}
+//
+//	puts("Enter words to add to the file:press the enter");
+//	puts("Key at the beginning of a line to terminate.");
+//	
+//	while (gets_s(words) != NULL && words[0] != '\0') 
+//	{
+//		//从标准键盘获取字符串---get string from the stdin 
+//		fprintf(fp, "%s ", words);
+//	}
+//
+//	puts("File contens: ");
+//	rewind(fp);  // 回到文件的开始处---get bace the beginning of file  
+//
+//	while (fscanf(fp, "%s", words) == 1) 
+//	{
+//		//一个一个字符串输出----output in each string 
+//		puts(words);
+//	}
+//
+//	if (fclose(fp) != 0)
+//	{
+//		fprintf(stderr, "Error closing file\n");
+//	}
+//
+//	return 0;
+//}
+
+
+#define N 15
+#define LEN 15
+
+struct names
 {
-	FILE* fp;
-	char words[MAX];
+	char fname[N];
+	char mname[N];
+	char lname[N];
 
-	if ((fp = fopen("words", "a+")) == NULL)
+};
+
+struct messages
+{
+	char ints_num[LEN];
+	struct names name;
+};
+
+char* s_gets(char* st, int n)
+{
+	char* ret_val;
+	char* find;
+
+	ret_val = fgets(st, n, stdin);
+
+	if (ret_val)
 	{
-		fprintf(stderr, "Can't open \"words\" file.\n");
-		exit(1);
+		find = strchr(st, '\n');
+
+		if (find)
+		{
+			*find = '\0';
+		}
+		else
+		{
+			while (getchar() != '\n')
+			{
+				continue;
+			}
+		}
 	}
 
-	puts("Enter words to add to the file:press the enter");
-	puts("Key at the beginning of a line to terminate.");
-	
-	while (gets_s(words) != NULL && words[0] != '\0') 
+	return ret_val;
+
+}
+
+void show_messages(const struct messages pt)
+{
+	if (pt.name.mname[0] == '\0')
 	{
-		//从标准键盘获取字符串---get string from the stdin 
-		fprintf(fp, "%s ", words);
+		printf("%s,%s", pt.name.fname, pt.name.lname);
+		printf(" -- %s\n", pt.ints_num);
+	}
+	else
+	{
+		printf("%s,%s %c.", pt.name.fname, pt.name.lname, pt.name.mname[0]);
+		printf(" -- %s\n", pt.ints_num);
 	}
 
-	puts("File contens: ");
-	rewind(fp);  // 回到文件的开始处---get bace the beginning of file  
+	return;
+}
 
-	while (fscanf(fp, "%s", words) == 1) 
+
+int main(void)
+{
+	int i = 0;
+	int count = 0;
+	struct messages m[5];
+
+	printf("Please enter the insurance number:\n");
+	printf("Please [enter] at the start of a line to stop.\n");
+
+	while (count < 5 && s_gets(m[count].ints_num, LEN) && m[count].ints_num[0] != '\n')
 	{
-		//一个一个字符串输出----output in each string 
-		puts(words);
+		printf("Now enter the former name:\n");
+		s_gets(m[count].name.fname, N);
+		printf("Now enter the middle name(without,[enter] to the next):\n");
+		s_gets(m[count].name.mname, N);
+		printf("Now enter last name:\n");
+		s_gets(m[count].name.lname, N);
+
+		if (count++ < 5)
+		{
+			printf("Enter the next insurance number:\n");
+		}
 	}
 
-	if (fclose(fp) != 0)
+	if (count > 0)
 	{
-		fprintf(stderr, "Error closing file\n");
+		printf("All number messages:\n");
+		for (i = 0; i < count; i++)
+		{
+			show_messages(m[i]);
+		}
 	}
-
+	else
+	{
+		printf("No data!\n");
+	}
 	return 0;
 }
+
+// 编写一个程序满足下面的要求
+// a. name 结构体，两个成员：名 and 姓
+// b. student 结构体，三个成员：name grade[(float)[3]] 和 一个变量存储3个分数平均数
+// c. main()函数中声明 一个内含CSIZE(CSIZE = 4)个student 类型结构的数组，
+//    并初始化这些结构的名字部分。
+// d.获取每个学生的成绩，提示用户输入学生的姓名和分数，分数存储到 grade 数组相应的结构中。
+// 可在main()函数或其他函数中用循环来完成
+// e. 计算每个结构体的平均分，赋给合适的成员
+// f. 打印每个结构的信息
+// g. 打印班级的平均分，即所有结构的 数值成员的平均值
+//
+
+
+
+
+
+
+
 
