3ba6adf4a4270c927e17b9118b14cb9c54e3d3db
1SYF666 <2832195814@qq.com>
2023-12-25 09:32:21 +0800

    fifth

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -16,43 +16,6 @@ using namespace std;
 
 int main()
 {
-	string fileName;
-	
-	cout << "Please enter of file to open: ";
-	cin >> fileName;
-
-	if (fileName.empty())
-	{
-		cerr << "fileName is empty().bailing out.bye!\n";
-		return -1;
-	}
-
-	ifstream inFile(fileName.c_str());  
-
-	if (!inFile)
-	{
-		cerr << "unable to open file.bailing out.bye!\n";
-		return -2;
-	}
-
-	string inBuf;
-	vector<string> text;
-
-	while (inFile>>inBuf)
-	{
-		for (int ix = 0; ix < inBuf.size(); ++ix)
-		{
-			if ( (char ch = inBuf[ix]) == '.')
-			{
-				ch = '_';
-
-			}
-
-		}
-
-	}
-
-
 
 
 
