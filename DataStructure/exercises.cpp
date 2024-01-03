876e00979d48cfc82c91ee7e4d2fe3b4749c9413
1SYF666 <2832195814@qq.com>
2024-01-03 14:35:21 +0800

    sixth

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -2,9 +2,12 @@
 #include <cassert>
 #include <fstream>
 #include <string>
+#include <utility> 
 #include <complex>
 #include <vector>
+#include <algorithm>
 #include <map>
+#include <set>
 using namespace std;
 //C++标准库中的名字都是在一个称作 std 的名字空间中声明的
 //using 指示符告诉编译器要使用在名字空间 std 中声明的名字
@@ -14,102 +17,124 @@ using namespace std;
 // __FILE__ 用以指示本行语句所在源文件的文件名
 // __LINE__ 用以指示本行语句所在源文件中的位置信息
 
+typedef pair<short, short> location;
+typedef vector<location, allocator> loc;
+typedef vector<string, allocator> text;
+typedef pair<text*, loc*> text_loc;
 
+
+class TextQuery
+{
+public:
+	TextQuery()
+	{
+		memset(this, 0, sizeof(TextQuery));
+	}
+	static void filter_elements(string felems)
+	{
+		filt_elems = felems;
+	}
+	void query_text();
+	void display_map_text();
+	void display_text_locations();
+	void doit() 
+	{
+		retrieve_text();
+		separate_words();
+		filter_text();
+		suffix_text();
+		strip_caps();
+		build_word_map();
+	}
+
+private:
+	void retrieve_text();
+	void separate_words();
+	void filter_text();
+
+	void strip_caps();
+	void suffix_text();
+	void suffix_s(string&);
+	void build_word_map();
+
+private:
+	vector<string, allocator>* lines_of_text;
+	text_loc* text_locations;
+	map<string, loc*, less<string>, allocator>* word_map;
+	static string filt_elems;
+};
+
+
+string TextQuery::filt_elems("\",.;:!<<)(\\/");
 
 int main()
 {
-	map<string, string> trans_map;
-	typedef map<string, string>::value_type valType;
 
-	//第一个权宜之计：将转换对固定写在代码中
-	trans_map.insert(valType("gratz", "grateful"));
-	trans_map.insert(valType("em", "them"));
-	trans_map.insert(valType("cuz", "because"));
-	trans_map.insert(valType("nah", "no"));
-	trans_map.insert(valType("sez", "says"));
-	trans_map.insert(valType("tanx", "thanks"));
-	trans_map.insert(valType("wuz", "was"));
-	trans_map.insert(valType("pos", "suppose"));
-
-	// OK:显示trans_map
-	map<string, string>::iterator it;
-	cout << "Here is our transformation map: \n\n";
-
-	for (it = trans_map.begin(); it != trans_map.end(); ++it)
-	{
-		cout << "key: " << (*it).first << "\t"
-			<< "value: " << (*it).second << "\n";
-	}
-	cout << "\n\n";
-
-	//第二个权宜之计：固定写入文字
-	string textarray[14] = { "nah", "I", "sez", "tanx", "cuz", "I","wuz",
-							 "pos", "to", "not", "cuz", "I", "wuz", "gratz" };
-	vector<string> text(textarray, textarray + 14);
-	vector<string>::iterator iter;
-
-	// ok:显示text
-	cout << "Here is our original string vector: \n\n";
-	int cnt = 1;
-	for (iter = text.begin(); iter != text.end(); ++iter, ++cnt)
-	{
-		cout << *iter << (cnt % 8 ? " " : "\n");
-	}
-	cout << "\n\n\n";
-
-	//包含统计信息的map——动态生成
-	map<string, int>stats;
-	typedef map<string, int>::value_type statsValType;
-
-	//ok:真正的map 工作——程序的核心
-	for (iter = text.begin(); iter != text.end(); ++iter)
-	{
-		if ((it = trans_map.find(*iter)) != trans_map.end())
-		{
-			if (stats.count(*iter))
-			{
-				stats[*iter] += 1;
-			}
-			else
-			{
-				stats.insert(statsValType(*iter, 1));
-			}
-			*iter = (*it).second;
-		}
-	}
-
-	//ok: 显示被转换后的vector
-	cout << "Here is our transformed string vector: \n\n";
-	cnt = 1;
-
-	for (iter = text.begin(); iter != text.end(); ++iter, ++cnt)
-	{
-		cout << *iter << (cnt % 8 ? " ":"\n");
-	}
-
-	cout << endl << endl;
-
-	//ok: 现在对统计map进行迭代
-	cout << "Finally,here are our statistic:\n\n";
-	//map<string, int, less<string>, allocator>::iterator siter;
-	map<string, int, less<string>>::iterator siter;
-
-	for (siter = stats.begin(); siter != stats.end(); ++siter)
-	{
-		cout << (*siter).first << " "
-			<< "was transformed "
-			<< (*siter).second
-			<< ((*siter).second == 1 ?
-			 " time\n":" times\n");
-	}
-
-
-
-
-
+	TextQuery tq;
+	tq.doit();
+	tq.query_text();
+	tq.display_map_text();
 
 	return 0;
 }
+
+
+void TextQuery::retrieve_text()
+{
+	string file_name;
+	cout << "Please enter file name: ";
+	cin >> file_name;
+	ifstream infile(file_name.c_str(), ios::in);
+
+	if (!infile)
+	{
+		cerr << "oops! unable to open file "
+			<< file_name << " -- bailing out!\n";
+		exit(-1);
+	}
+
+	else cout << "\n";
+
+	lines_of_text = new vector<string, allocator>;
+
+	string textline;
+
+	while (getline(infile,textline,'\n'))
+	{
+		lines_of_text->push_back(textline);
+	}
+}
+
+void TextQuery::separate_words()
+{
+	vector<string, allocator>* word = new vector<string, allocator>;
+	vector<location, allocator>* location = new vector<location, allocator>;
+
+	for (short line_pos = 0; line_pos < lines_of_text->size(); line_pos++)
+	{
+		short word_pos = 0;
+		string textline = (*lines_of_text)[line_pos];
+		string::size_type eol = textline.length();
+		string::size_type pos = 0, prev_pos = 0;
+		while ((pos=textline.find_first_of(' ',pos))!=string::npos)
+		{
+			words->push_back(
+				textline.substr(prev_pos, pos - prev_pos));
+			locations->push_back(
+				make_pair(line_pos, word_pos));
+			word_pos++; pos++; prev_pos = pos;
+		}
+		words->push_back(
+			textline.substr(prev_pos, pos - prev_pos));
+		locations->push_back(make_pair(line_pos, word_pos));
+
+	}
+
+	text_locations = new text_loc(words, locations);
+}
+
+
+
 
 
 
