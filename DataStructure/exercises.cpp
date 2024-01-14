a8ca0dd28b1c0766cefdfca00668aaeda04bec36
1SYF666 <2832195814@qq.com>
2024-01-14 23:01:21 +0800

    seventh

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -23,34 +23,25 @@ using namespace std;
 
 const int YARD_PER_LONG = 220;
 
+void show_time(int hour, int minute);
+
 int main()
 {
-	map<string, size_t>word_count;
-	set<string> exclude = { "The","But","And","Or","An","A",
-							 "the","but","and","or","an","a" };
+	int hour, minute;
 
-	string word;
-
-	while (cin >> word)
-	{
-		//只统计不在exclude中的单词
-		if (exclude.find(word) == exclude.end())
-		{
-			++word_count[word];
-		}
-	}
-
-	for (const auto& w : word_count)
-	{
-		cout << w.first << " occurs " << w.second
-			<< ((w.second > 1) ? " times" : " time")
-			<< endl;
-	}
-
+	cout << "Enter the number of hours: ";
+	cin >> hour;
+	cout << "Enter the number of minutes: ";
+	cin >> minute;
+	show_time(hour, minute);
 
 	return 0;
 }
 
+void show_time(int hour, int minute)
+{
+	cout << "Time: " << hour << ":" << minute << endl;
+}
 
 
 
