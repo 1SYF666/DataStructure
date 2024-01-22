fcd8d5c2451204d92f2fd114c07f826ac90ab6d9
1SYF666 <2832195814@qq.com>
2024-01-22 22:16:29 +0800

    seventh

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -21,27 +21,21 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-    int age;
-    char grade;
-    string fname, lname;
+    string name, dessert;
 
-    cout << "What is your first name? ";
-    getline(cin, fname);
-    cout << "What is your last name? ";
-    getline(cin, lname);
-    cout << "What letter grade do you deserve? ";
-    cin >> grade;
-    cout << "What is your age? ";
-    cin >> age;
-
-    cout << "Name: " << lname << ", " << fname << endl;
-    cout << "Grade: " << ++grade << endl;
-    cout << "Age: " << age << endl;
+    cout << "Enter your name:\n";
+    getline(cin, name);
+    cout << "Enter your favorite dessert:\n";
+    getline(cin, dessert);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
 
     return 0;
 }
+
 
 
 
