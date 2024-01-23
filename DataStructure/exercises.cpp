c50e4cc3c7f3b1d717038e1035bc731900bbbb24
1SYF666 <2832195814@qq.com>
2024-01-23 22:16:25 +0800

    seventh

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -21,17 +21,26 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
+const int ArSize = 20;
 
 int main()
 {
-    string name, dessert;
+    char fname[ArSize];
+    char lname[ArSize];
 
-    cout << "Enter your name:\n";
-    getline(cin, name);
-    cout << "Enter your favorite dessert:\n";
-    getline(cin, dessert);
-    cout << "I have some delicious " << dessert;
-    cout << " for you, " << name << ".\n";
+    cout << "Enter your first name: ";
+    cin.getline(fname, ArSize);
+    cout << "Enter your last name: ";
+    cin.getline(lname, ArSize);
+
+    char* name = new char[strlen(fname) + strlen(lname) + 5];
+    strcpy(name, lname);
+    strcat(name, ", ");
+    strcat(name, fname);
+
+    cout << "Here's the information in a single string: ";
+    cout << name << endl;
+    delete[] name;
 
     return 0;
 }
