76da8ce51f0dc9191d18127d75f3f70e9d7e177d
1SYF666 <2832195814@qq.com>
2024-01-29 22:23:33 +0800

    seventh

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -19,7 +19,8 @@ using namespace std;
 // __LINE__ 用以指示本行语句所在源文件中的位置信息
 
 
-// 4 -5
+// 4 -6
+#include <iostream>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -33,11 +34,26 @@ struct CandyBar
 
 int main()
 {
-    CandyBar snack{ "Mocha Munch", 2.3, 350 };
+    CandyBar snack[3] = {
+        {"Mocha Munch", 2.3, 350},
+        {"Mooncake", 3.5, 369},
+        {"Birthdaycake", 6.8, 460},
+    };
 
-    cout << "Brand : " << snack.brand << endl;
-    cout << "Candy_weight: " << snack.candy_weight << endl;
-    cout << "Candy_calorie: " << snack.candy_calorie << endl;
+    cout << "The first candy:" << endl;
+    cout << "Brand: " << snack[0].brand << endl;
+    cout << "Candy_weight: " << snack[0].candy_weight << endl;
+    cout << "Candy_calorie: " << snack[0].candy_calorie << endl;
+
+    cout << "\nThe second candy:" << endl;
+    cout << "Brand: " << snack[1].brand << endl;
+    cout << "Candy_weight: " << snack[1].candy_weight << endl;
+    cout << "Candy_calorie: " << snack[1].candy_calorie << endl;
+
+    cout << "\nThe third candy:" << endl;
+    cout << "Brand: " << snack[2].brand << endl;
+    cout << "Candy_weight: " << snack[2].candy_weight << endl;
+    cout << "Candy_calorie: " << snack[2].candy_calorie << endl;
 
     return 0;
 }
