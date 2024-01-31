a1c39f93800ea2487717d2c3119966df33928277
1SYF666 <2832195814@qq.com>
2024-01-31 22:43:34 +0800

    seventh

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -19,7 +19,7 @@ using namespace std;
 // __LINE__ 用以指示本行语句所在源文件中的位置信息
 
 
-// 4 -6
+// 4 -8
 #include <iostream>
 #include <string>
 using namespace std;
@@ -33,18 +33,19 @@ struct Pizza
 
 int main()
 {
-    Pizza snack;
+    Pizza* snack = new Pizza;
 
-    cout << "Please enter the name of the Pizza Company: ";
-    getline(cin, snack.pizza_company);
     cout << "Please enter the diameter of the pizza: ";
-    cin >> snack.pizza_diameter;
+    (cin >> snack->pizza_diameter).get();
+    cout << "Please enter the name of the Pizza Company: ";
+    getline(cin, snack->pizza_company);
     cout << "Please enter the weight of the pizza: ";
-    cin >> snack.pizza_weight;
+    cin >> snack->pizza_weight;
 
-    cout << "The name of the Pizza Company: " << snack.pizza_company << endl;
-    cout << "The diameter of the Pizza Company: " << snack.pizza_diameter << endl;
-    cout << "The weight of the Pizza Company: " << snack.pizza_weight << endl;
+    cout << "The name of the Pizza Company: " << snack->pizza_company << endl;
+    cout << "The diameter of the Pizza Company: " << snack->pizza_diameter << endl;
+    cout << "The weight of the Pizza Company: " << snack->pizza_weight << endl;
+    delete snack;
 
     return 0;
 }
