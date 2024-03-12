8cf737de85f2bc8b6c420c8c0d8392535a531fc2
1SYF666 <2832195814@qq.com>
2024-03-12 21:34:10 +0800

    eighth

--- DataStructure/exercises.cpp
+++ DataStructure/exercises.cpp
@@ -23,30 +23,21 @@ using namespace std;
 #include <iostream>
 using namespace std;
 
-void print_string(const char* str, int n = 0);
-
 int main()
 {
-    const char* str = "This is a test.";
+	enum color
+	{
+		RED,
+		GREEN,
+		BLUE
+	}backColor, frontColor, innerColor;
 
-    cout << "Only one parameter, print 1 times:" << endl;
-    print_string(str);
-    cout << "Having two parameter can print 2 times:" << endl;
-    print_string(str, 3);
-    cout << "Bye." << endl;
+	backColor = RED;
+	frontColor = GREEN;
+	innerColor = BLUE;
 
-    return 0;
+	cout << backColor << "--" << frontColor << "--" << innerColor << endl;
+	system("pause");
+	return 0;
 }
-
-void print_string(const char* str, int n)
-{
-    static int count = 1;
-
-    for (int i = 0; i < count; ++i)
-    {
-        cout << str << endl;
-    }
-    ++count;
-}
-
 
